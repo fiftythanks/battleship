91fa3f3340c7cf84607ec551668a34db35069a8f
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-04-23 23:50:50 +0700

    chore: edit wording in test descriptions

---
+++ src/Battleship.mjs
@@ -0,0 +1,35 @@
+/* 
+  1. Begin your app by creating the Ship class/factory (your choice).
+
+    1. Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
+
+    2. REMEMBER you only have to test your object’s public interface. Only methods or properties that are used outside of your ‘ship’ object need unit tests.
+    
+    3. Ships should have a hit() function that increases the number of ‘hits’ in your ship.
+    
+    4. isSunk() should be a function that calculates whether a ship is considered sunk based on its length and the number of hits it has received.
+*/
+
+export default class Battleship {
+  constructor(length) {
+    if (
+      typeof length !== 'number' ||
+      !Number.isInteger(length) ||
+      length < 1 ||
+      length > 5
+    )
+      throw new TypeError(
+        'Incorrect length! Length must be an integer number from 1 to 5. Any other value will result in an error.',
+      );
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
