d79f80fe5ef639c0557d952a609cb17bf9cfe70f
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-04-24 23:00:41 +0700

    test: add test for Game attack methods

--- src/modules/Game.test.mjs
+++ src/modules/Game.test.mjs
@@ -8,4 +8,24 @@ describe('Game class', () => {
     expect(game.P1PlaceShip(['B', 1], ['F', 1])).toBeInstanceOf(Battleship);
     expect(game.P2PlaceShip(['B', 1], ['F', 1])).toBeInstanceOf(Battleship);
   });
+
+  it('can attack players', () => {
+    expect(game.attackPlayerOne('C', 4)).toBeNull();
+    expect(game.attackPlayerOne('B', 1)).toBeInstanceOf(Battleship);
+
+    expect(game.attackPlayerTwo('C', 4)).toBeNull();
+    expect(game.attackPlayerTwo('B', 1)).toBeInstanceOf(Battleship);
+
+    // Can't attack the same square twice
+    expect(() => game.attackPlayerOne('C', 4)).toThrow();
+    expect(() => game.attackPlayerTwo('C', 4)).toThrow();
+  });
+
+  /* 
+    board.placeShip(['B', 1], ['F', 1]);
+    board.placeShip(['C', 4], ['C', 7]);
+    board.placeShip(['E', 3], ['E', 5]);
+    board.placeShip(['G', 6], ['H', 6]);
+    board.placeShip(['J', 4]); 
+  */
 });
