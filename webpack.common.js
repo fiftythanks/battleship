dcc5c0b03456d0b9da7eb8dc8c5e4a1651a01b14
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-03 17:18:13 +0700

    chore: fix webpack.common.js
    Before, webpack didn't work properly with ESM.

--- webpack.common.js
+++ webpack.common.js
@@ -34,6 +34,15 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.json'],
   },
 };
